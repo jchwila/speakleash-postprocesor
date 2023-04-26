62bf7225ed81918c735d1fe941f29989f7546cb3
jchwila <jacek.chwila@gmail.com>
2023-04-26 19:03:18 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -10,10 +10,15 @@ import shutil
 import spacy
 from rich import print as rich_print
 from common.functions import log
+from multiprocessing import Pool, set_start_method
+
 
 if __name__ == '__main__':
 
-    VERSION = "0.1.0"
+   
+    VERSION = "0.1.1"
+
+    nlp = spacy.load("pl_core_news_md", disable=('ner','textcat','entity_linker'))
 
     base_dir = os.path.join(os.path.dirname(__file__))
     manifest_dir = os.path.join(base_dir, "manifests")
@@ -29,14 +34,16 @@ if __name__ == '__main__':
     parser.add_argument("--metrics", action="store_true", help="Calculate metrics for dataset")
     parser.add_argument("--name", type=str, help="Name of dataset")
 
+
     args = parser.parse_args()
 
-    all = True
+    all = not args.name
 
+ 
     #Set defaults
-    args.sample = False
-    args.metrics = True
-    args.name = "1000_novels_corpus_CLARIN-PL"
+    #args.sample = False
+    #args.metrics = True
+
 
     if args.metrics:
         if not os.path.exists(manifest_dir):
@@ -62,98 +69,101 @@ if __name__ == '__main__':
 
     print("")
     log("Starting post-processing", "INFO")
-    nlp = spacy.load("pl_core_news_md", disable=('ner','lemmatizer','textcat','entity_linker', 'attribute_ruler'))
+    
     manifest = {}
     sl = Speakleash(replicate_to)
     for d in sl.datasets:
         if all or d.name == args.name:
             log("Processing dataset: " + d.name, "INFO")
 
-            total_len = 0
-            total_docs = 0
-            total_sentences = 0
-            total_words = 0
-            total_verbs = 0
-            total_nouns = 0
-            total_punctuations = 0
-            total_symbols = 0
-            total_stopwords = 0
-            total_oovs = 0
-            total_adjecives = 0
-            total_adverbs = 0
+            stats = {'documents': 0}
 
             manifest = d.manifest
 
-            ar = Archive(os.path.join(base_dir, "data"))
+            
             file_name_zst = os.path.join(base_dir, d.name + '.zst')
             file_name_manifest = os.path.join(base_dir, d.name + '.manifest') 
 
             counter = 0
             sample = []
-            ds =d.ext_data
+            ds = d.ext_data
 
-            for doc in ds:
-                txt, meta = doc
+            if args.metrics:
+                ar = Archive(os.path.join(base_dir, "data"))
                 
-                if args.metrics:
-                    analyzer = Analyzer(txt, meta, nlp, counter)
+                for nlp_doc, ds_doc in zip(nlp.pipe(doc[0] for doc in ds), ds):
+                                     
+                    analyzer = Analyzer(ds_doc[0], ds_doc[1], nlp_doc, counter)
                     meta = analyzer.go()
-
-                    total_words += meta['words']
-                    total_verbs += meta['verbs']
-                    total_nouns += meta['nouns']
-                    total_len += meta['length']
-                    total_docs += 1
-                    total_sentences += meta['sentences']
-                    total_punctuations += meta['punctuations']
-                    total_symbols += meta['symbols']
-                    total_stopwords += meta['stopwords']
-                    total_oovs += meta['oovs']   
-                    total_adjecives += meta['adjecives']
-                    total_adverbs += meta['adverbs'] 
-
+                    stats['documents'] += 1                        
+                    
+                    for key in meta.keys():
+                        if not isinstance(meta[key], str):
+                            stats[key] = stats.setdefault(key, 0) + meta[key]
                     ar.add_data(txt, meta = meta)                
 
-                counter += 1
-
-                if args.metrics:
-                    ar.commit()
                     
-                if args.sample:
-                    if counter <= 5:
-                        sample.append({"text": txt, "meta": meta})
-                
-                    if counter == 5:
-                        with open(os.path.join(base_dir, sample_dir, d.name + ".sample"), "w") as f:
-                            f.write(json.dumps(sample, ensure_ascii = False ,  indent=4))
+
+                    if args.sample:
+                        if counter < 5:
+                            sample.append({"text": txt, "meta": meta})
+            
+                        if counter == 4:
+                            with open(os.path.join(base_dir, sample_dir, d.name + ".sample"), "w", encoding = "utf-8") as f:
+                                f.write(json.dumps(sample, ensure_ascii = False ,  indent=4))
+
+                    counter += 1
+
+
+
+               
+                ar.commit()
+
+                for key in stats.keys():
+                    if key in Analyzer.AVG_METRICS_DEF:
+                        stats[key] = round(stats[key]/stats['documents'],6)
     
-            if args.metrics:
-                ar = None
-                data_files= glob.glob(os.path.join('data','/*'))
-                file_size = 0
                 
+                ar = None
+                data_files= glob.glob(os.path.join(base_dir,'data','*'))
+                file_size = 0
+
                 for f in data_files:
                     if f.endswith('.zst'):
-                        shutil.copy(f, os.path.join(file_name_zst))
+                        shutil.copy(f, file_name_zst)
                         file_size = os.path.getsize(file_name_zst)
                         os.remove(f)
 
-                stats = {"documents": total_docs, "sentences": total_sentences, "words" : total_words, "nouns" : total_nouns, "verbs" : total_verbs, "characters": total_len, "punctuations" : total_punctuations, "symbols" : total_symbols, 'stopwords': total_stopwords,  'oovs': total_oovs, 'adjecives': total_adjecives, 'adverbs': total_adverbs}
                 manifest['stats'] = stats
-                manifest['size'] = file_size
+                manifest['file_size'] = file_size
             
                 json_manifest = json.dumps(manifest, indent = 4) 
-                with open(os. file_name_manifest, 'w') as mf:
+                with open(file_name_manifest, 'w') as mf:
                     mf.write(json_manifest)
-            
-            if os.path.exists(os.path.join(replicate_to, d.name + '.jsonl.zst')):
-                os.remove(os.path.join(replicate_to, d.name + '.jsonl.zst'))
 
-            if os.path.exists(os.path.join(replicate_to, d.name + '.manifest')):
-                os.remove(os.path.join(replicate_to, d.name + '.manifest'))
- 
-            if os.path.exists(os.path.join('data')):
-                shutil.rmtree(os.path.join('data'))
+            if not args.metrics and args.sample:
+                
+            
+                for i in range(5):
+                    if i < 5:
+                        txt, meta = next(ds)
+                        sample.append({"text": txt, "meta": meta})
+                                        
+                    if i == 4:
+                        with open(os.path.join(base_dir, sample_dir, d.name + ".sample"), "w", encoding="utf-8") as f:
+                            f.write(json.dumps(sample, ensure_ascii = False,  indent=4))
+                
+                ds = None
+
+            
+        if os.path.exists(os.path.join(replicate_to, d.name + '.jsonl.zst')):
+            os.remove(os.path.join(replicate_to, d.name + '.jsonl.zst'))
+
+        if os.path.exists(os.path.join(replicate_to, d.name + '.manifest')):
+            os.remove(os.path.join(replicate_to, d.name + '.manifest'))
+
+        if os.path.exists(os.path.join('data')):
+            shutil.rmtree(os.path.join('data'))
 
     log("Finished post-processing", "INFO")
     print("")
--- postprocessor/analyzer.py
+++ postprocessor/analyzer.py
@@ -4,12 +4,13 @@ import textstat
 
 class Analyzer(object):
 
-    def __init__(self, txt, meta, nlp, index):
+    AVG_METRICS_DEF = ['avg_word_length', 'avg_sentence_length', 'noun_ratio', 'verb_ratio', 'adj_ratio', 'lexical_density', 'gunning_fog']
+
+    def __init__(self, txt, meta, doc, index):
         textstat.set_lang('pl')
         self.txt = txt
         self.meta = meta
-        self.nlp = nlp
-        self.nlp.max_length = len(txt) + 100
+        self.doc = doc
         self.index = index
 
     def go(self):
@@ -22,7 +23,7 @@ class Analyzer(object):
 
         new_meta = self.meta
 
-        doc = self.nlp(self.txt)
+                
         words = 0
         verbs = 0
         nouns = 0
@@ -30,20 +31,20 @@ class Analyzer(object):
         symbols = 0
         stopwords = 0
         oovs = 0
-        adjecives = 0
+        adjectives = 0
         adverbs = 0
         avg_word_length = 0
         avg_sentence_length = 0
         sentences = 0
-        noun_freq = 0
-        verb_freq = 0
-        adj_freq = 0
+        noun_ratio = 0
+        verb_ratio = 0
+        adj_ratio = 0
         lexical_density = 0
         gunning_fog = 0
         uniq_words = set()
-        words_list = []
+        
 
-        for token in doc:
+        for token in self.doc:
             if not token.is_punct and not token.is_stop and not token.is_space:
                 if token.is_oov and not token.pos_ == "SYM":
                     oovs +=1
@@ -54,7 +55,7 @@ class Analyzer(object):
                     verbs += 1
                     uniq_words.add(token.lemma_)
                 elif token.pos_ == "ADJ":
-                    adjecives += 1
+                    adjectives += 1
                     uniq_words.add(token.lemma_)
                 elif token.pos_ == "ADV":
                     adverbs += 1
@@ -68,9 +69,9 @@ class Analyzer(object):
                 punctuations += 1
             elif not token.is_space and not token.pos_ == "SYM":
                 words += 1
-                words_list.append(token.lemma_)
+                
 
-        for sentence in doc.sents:
+        for sentence in self.doc.sents:
             avg_sentence_length += len(sentence)
             sentences += 1
 
@@ -85,26 +86,26 @@ class Analyzer(object):
             avg_word_length = 0
 
         if words > 0:
-            noun_freq = nouns / words
+            noun_ratio = nouns / words
         else:
-            noun_freq = 0
+            noun_ratio = 0
 
         if words > 0:
-            verb_freq = verbs / words
+            verb_ratio = verbs / words
         else:
-            verb_freq = 0   
+            verb_ratio = 0   
 
         if words > 0:
-            adj_freq = adjecives / words
+            adj_ratio = adjectives / words
         else:
-            adj_freq = 0
+            adj_ratio = 0
 
-        if len(words_list) > 0:
-            lexical_density = len(uniq_words) / len(words_list)
+        if words > 0:
+            lexical_density = len(uniq_words) / words
         else:
             lexical_density = 0
 
-        if len(words_list) > 0:
+        if words > 0:
             gunning_fog = textstat.gunning_fog(self.txt)
 
 
@@ -115,22 +116,22 @@ class Analyzer(object):
         new_meta["verbs"] = verbs
         new_meta["nouns"] = nouns
         new_meta["adverbs"] = adverbs
-        new_meta["adjecives"] = adjecives
+        new_meta["adjectives"] = adjectives
         new_meta["punctuations"] = punctuations
         new_meta["symbols"] = symbols
         new_meta["stopwords"] = stopwords
         new_meta["oovs"] = oovs
         new_meta["avg_word_length"] = avg_word_length
-        new_meta["noun_freq"] = noun_freq
-        new_meta["verb_freq"] = verb_freq
-        new_meta["adj_freq"] = adj_freq
+        new_meta["noun_ratio"] = noun_ratio
+        new_meta["verb_ratio"] = verb_ratio
+        new_meta["adj_ratio"] = adj_ratio
         new_meta["lexical_density"] = lexical_density
         new_meta["gunning_fog"] = gunning_fog
 
 
         d = datetime.now() - t1
-        elapsed = round(d.microseconds / 1000)
-        log("Processing document (" + str(elapsed) + " ms): " + str(self.index+1) + " " + name, "INFO")
+        elapsed = d.total_seconds()
+        log("Processing document (" + str(elapsed) + " s): " + str(self.index+1) + " " + name, "INFO")
 
         return new_meta
 
